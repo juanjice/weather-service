4ab02d22de220c09c74f38fc30a608383433242c
juanjice <juanjimenezcelisj2@gmail.com>
2021-09-19 22:12:53 -0500

    Add documentation

--- src/main/java/com/weatherexample/consumeapi/ConsumeapiApplication.java
+++ src/main/java/com/weatherexample/consumeapi/ConsumeapiApplication.java
@@ -1,5 +1,9 @@
+/**
+*
+* @author Juan Manuel Jimenez Celis
+*
+*/
 package com.weatherexample.consumeapi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumeapiApplication {
-
+    //create a bean with RestTemplate
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
--- src/main/java/com/weatherexample/consumeapi/weather/WeatherController.java
+++ src/main/java/com/weatherexample/consumeapi/weather/WeatherController.java
@@ -12,21 +12,25 @@ import java.util.Optional;
 @RestController
 @CrossOrigin("*")
 public class WeatherController {
-
+    //use rest template to consume public api, and inyect the appropiate bean
     @Autowired
     private RestTemplate restTemplate;
     private final String API_KEY="1814d49f2dd7f3a9d477e486ee113895";
+    //pdf tell us that only Bogota information is required
     private static String url ="https://api.openweathermap.org/data/2.5/weather?q=Bogota";
-
+    //end point for the rest going to be /weather
     @GetMapping("/weather")
     @ResponseBody
     public Object getWeather(@RequestParam(value = "units",defaultValue = "standard") Optional<String> units,
-                             @RequestParam(value = "lang",defaultValue = "es") Optional<String> lang){//
+                             @RequestParam(value = "lang",defaultValue = "es") Optional<String> lang){
+        //build UriComponentsBuilder with the static metod from UriString, and add the params in the request
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
                                         .queryParam("appid",API_KEY)
                                         .queryParam("units",units)
                                         .queryParam("lang",lang);
+        //get template from uri
         Object response = restTemplate.getForObject(builder.toUriString(),Object.class);
+        //return an object
         return response;
     }
     
